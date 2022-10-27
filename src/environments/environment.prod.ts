f15838c95c6d64ae647d13bb389d5b3474f8e173
ktt45678 <ktt45678@gmail.com>
2022-10-27 19:42:30 +0700

    Update environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev.kamplexapi.tk/api',
-  socketUrl: 'https://dev.kamplexapi.tk/ws',
+  apiUrl: 'https://api.kamplex.tk/api',
+  socketUrl: 'https://api.kamplex.tk/ws',
   consumetApiUrl: 'https://api.consumet.org',
   recaptchaSiteKey: '6LdgGTEiAAAAAMYx6INUKD61OrbseK8tN3Lqki0R'
 };
